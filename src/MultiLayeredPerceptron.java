fb1eb349276167fae6e8f3079ff0cb9f71c07b02
ShaneWed <shanewedlock03@gmail.com>
2024-12-03 16:10:28 +0000

    Initial commit, fixed up stupid branch issues

---
+++ src/MultiLayeredPerceptron.java
@@ -0,0 +1,9 @@
+public class MultiLayeredPerceptron {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+    public MultiLayeredPerceptron(int[] inputs, int[] outputs, int[] layers, double learningRate) {
+
+    }
+}
